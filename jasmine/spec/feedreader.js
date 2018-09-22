cee2998e3918a302f66ac6ac92b866ad6ffdc33e
Lindsay Nunez <lindsayanunez@gmail.com>
2018-09-22 14:26:51 -0400

    Added test to ensure feed updates.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -57,7 +57,7 @@ $(function() {
 
     /* TODO: Write a new test suite named "The menu" */
 
-    describe('Menu', function(){
+    describe('The menu', function(){
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
@@ -87,6 +87,7 @@ $(function() {
      });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function(){
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -95,10 +96,43 @@ $(function() {
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
+         beforeEach(function(done){
+            loadFeed(0, done);
+         });
+
+         it('completes work', function(){
+            const feed = document.querySelector('.feed');
+            expect(feed.children.length > 0).toBe(true);
+         });
+
     /* TODO: Write a new test suite named "New Feed Selection" */
 
+    describe('New Feed Selection', function(){
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+
+        const feed = document.querySelector('.feed');
+        const firstFeed = [];
+
+         beforeEach(function(done){
+            loadFeed(0);
+            Array.from(feed.children).forEach(function(entry){
+                firstFeed.push(entry.innerText);
+            });
+            loadFeed(1, done);
+         });
+
+        it('content updates', function(){
+            Array.from(feed.children).forEach(function(entry,index){
+                expect(entry.innerText === firstFeed[index]).toBe(false);
+            });
+
+        });
+
+    });
+
+
+    });
 }());
