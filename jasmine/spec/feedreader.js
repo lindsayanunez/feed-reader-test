2107c3c20c8fda8eb88645aa44e62c0ca7e967f0
Lindsay Nunez <lindsayanunez@gmail.com>
2018-11-19 21:19:52 -0500

    Styled Javascript.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -8,12 +8,12 @@
  * since some of these tests may require DOM elements. We want
  * to ensure they don't run until the DOM is ready.
  */
-$(function() {
+$(function () {
     /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
-    describe('RSS Feeds', function() {
+     * a related set of tests. This suite is all about the RSS
+     * feeds definitions, the allFeeds variable in our application.
+     */
+    describe('RSS Feeds', function () {
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
          * empty. Experiment with this before you get started on
@@ -21,7 +21,7 @@ $(function() {
          * allFeeds in app.js to be an empty array and refresh the
          * page?
          */
-        it('are defined', function() {
+        it('are defined', function () {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
@@ -32,13 +32,13 @@ $(function() {
          * and that the URL is not empty.
          */
 
-         it('url defined', function() {
-            for(let feed of allFeeds){
+        it('url defined', function () {
+            for (let feed of allFeeds) {
                 expect(feed.url).toBeDefined();
                 expect(feed.url.length).not.toBe(0);
             }
 
-         });
+        });
 
 
         /* TODO: Write a test that loops through each feed
@@ -46,18 +46,18 @@ $(function() {
          * and that the name is not empty.
          */
 
-         it('name defined',function(){
-            for(let feed of allFeeds){
+        it('name defined', function () {
+            for (let feed of allFeeds) {
                 expect(feed.name).toBeDefined();
                 expect(feed.name.length).not.toBe(0);
             }
-         })
+        })
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
 
-    describe('The menu', function(){
+    describe('The menu', function () {
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
@@ -65,29 +65,29 @@ $(function() {
          * hiding/showing of the menu element.
          */
 
-        it('menu hidden', function(){
+        it('menu hidden', function () {
             const body = document.querySelector('body');
             expect(body.classList.contains('menu-hidden')).toBe(true);
 
         })
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+        /* TODO: Write a test that ensures the menu changes
+         * visibility when the menu icon is clicked. This test
+         * should have two expectations: does the menu display when
+         * clicked and does it hide when clicked again.
+         */
 
-          it('menu toggles', function(){
+        it('menu toggles', function () {
             const body = document.querySelector('body');
             const menu = document.querySelector('.menu-icon-link');
             menu.click();
             expect(body.classList.contains('menu-hidden')).toBe(false);
-          })
+        })
 
-     });
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
-    describe('Initial Entries', function(){
+    describe('Initial Entries', function () {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -96,42 +96,42 @@ $(function() {
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
-         beforeEach(function(done){
+        beforeEach(function (done) {
             loadFeed(0, done);
-         });
+        });
 
-         it('completes work', function(){
+        it('completes work', function () {
             const feed = document.querySelector('.feed');
             expect(feed.children.length > 0).toBe(true);
-         });
+        });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+        /* TODO: Write a new test suite named "New Feed Selection" */
 
-    describe('New Feed Selection', function(){
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
+        describe('New Feed Selection', function () {
+            /* TODO: Write a test that ensures when a new feed is loaded
+             * by the loadFeed function that the content actually changes.
+             * Remember, loadFeed() is asynchronous.
+             */
 
-        const feed = document.querySelector('.feed');
-        const firstFeed = [];
+            const feed = document.querySelector('.feed');
+            const firstFeed = [];
 
-         beforeEach(function(done){
-            loadFeed(0);
-            Array.from(feed.children).forEach(function(entry){
-                firstFeed.push(entry.innerText);
+            beforeEach(function (done) {
+                loadFeed(0);
+                Array.from(feed.children).forEach(function (entry) {
+                    firstFeed.push(entry.innerText);
+                });
+                loadFeed(1, done);
             });
-            loadFeed(1, done);
-         });
 
-        it('content updates', function(){
-            Array.from(feed.children).forEach(function(entry,index){
-                expect(entry.innerText === firstFeed[index]).toBe(false);
+            it('content updates', function () {
+                Array.from(feed.children).forEach(function (entry, index) {
+                    expect(entry.innerText === firstFeed[index]).toBe(false);
+                });
+
             });
 
         });
-
-    });
 
 
     });
